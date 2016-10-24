44c7de07a7a41cb9b4c7f6b38d97f13d34c681c5
ZhangKong <602488225@qq.com>
2016-10-24 11:51:15 +0800

    合并

--- Love_JiaYuan/Global.asax.cs
+++ Love_JiaYuan/Global.asax.cs
@@ -15,8 +15,6 @@ namespace Love_JiaYuan
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-#Love-JY
--------
-找呀找呀找朋友~~
-
-demo ：[love.niubile.me](http://love.niubile.me/)
-
-
-#电脑效果：
-![pc](http://i12.tietuku.com/5aaa6ac248a27393.png)
-
-
-
-#手机效果：
-![mb](http://i12.tietuku.com/2a3ec1ff99ecf177.png)
+# Love-JY  
